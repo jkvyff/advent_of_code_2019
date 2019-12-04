da9fa0771980835260739c0b7f1160023bdef23c
jkvyff <kristianvyff@gmail.com>
2019-12-04 14:50:51 -0800

    day-4 part two

--- day-4.go
+++ day-4.go
@@ -1,3 +1,20 @@
+/*
+--- Day 4: Secure Container ---
+You arrive at the Venus fuel depot only to discover it's protected by a password. The Elves had written the password on a sticky note, but someone threw it out.
+
+However, they do remember a few key facts about the password:
+
+It is a six-digit number.
+The value is within the range given in your puzzle input.
+Two adjacent digits are the same (like 22 in 122345).
+Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
+Other than the range rule, the following are true:
+
+111111 meets these criteria (double 11, never decreases).
+223450 does not meet these criteria (decreasing pair of digits 50).
+123789 does not meet these criteria (no double).
+How many different passwords within the range given in your puzzle input meet these criteria?
+*/
 package main
 
 import "fmt"
@@ -5,11 +22,13 @@ import "strconv"
 
 func main() {
 	lower, upper := 130254, 678275
-	count := 0
+	count, strictCount := 0, 0
 	for i := lower; i <= upper; i++ {
 		if valid(i) { count++ }
+		if strictValid(i) { strictCount++ }
 	}
 	fmt.Println("Part One:", count)
+	fmt.Println("Part Two:", strictCount)
 }
 
 func valid(num int) bool {
@@ -20,6 +39,38 @@ func valid(num int) bool {
 		num2, _ := strconv.Atoi(string(numString[i+1]))
 		if !(num1 <= num2) { return false }
 		if numString[i] == numString[i+1] { adjValues = true }
+	}
+	return adjValues
+}
+
+/*
+--- Part Two ---
+An Elf just remembered one more important detail: the two adjacent matching digits are not part of a larger group of matching digits.
+
+Given this additional criterion, but still ignoring the range rule, the following are now true:
+
+112233 meets these criteria because the digits never decrease and all repeated digits are exactly two digits long.
+123444 no longer meets the criteria (the repeated 44 is part of a larger group of 444).
+111122 meets the criteria (even though 1 is repeated more than twice, it still contains a double 22).
+How many different passwords within the range given in your puzzle input meet all of the criteria?
+*/
+
+func strictValid(num int) bool {
+	numString := strconv.Itoa(num)
+	adjValues := false
+	for i := 0; i < len(numString)-1; i++ {
+		num1, _ := strconv.Atoi(string(numString[i]))
+		num2, _ := strconv.Atoi(string(numString[i+1]))
+		if !(num1 <= num2) { return false }
+		if numString[i] == numString[i+1] {
+			if i == 0 {
+				if numString[i] != numString[i+2] { adjValues = true }
+			} else if i == len(numString) - 2 {
+				if numString[i] != numString[i-1] { adjValues = true }
+			} else if numString[i] != numString[i+2] && numString[i] != numString[i-1] {
+				adjValues = true
+			}
+		} 
 	}
 	return adjValues
 }
